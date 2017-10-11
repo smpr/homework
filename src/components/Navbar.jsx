fbaf03f92a89ee363ee9869467a93f88f6770a7d
smpr <gcbrown1116@gmail.com>
2017-10-11 13:56:57 -0400

    nav bar close to done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,61 @@
 import React, { Component } from 'react';
 import { FaInstagram, FaUser, FaHeartO, FaCompass, FaSearch } from 'react-icons/lib/fa';
 import styled from 'styled-components';
+const Wrapper = styled.div`
+background-color: white;
+display: flex;
+justify-content: space-between;
+padding: 26px 5vw;
+height: 35px;
+align-items: center;
+border-bottom: 1px solid rgba(0,0,0,.0975);
+
+`
+
+const LeftCol = styled.div`
+font-size: 30px;
+    margin: 0;
+    svg{
+      margin-bottom: 5px;
+    }
+
+`;
+const MiddleCol = styled.div`
+background: lightgray;
+border: 1px solid #dbdbdb;
+width: 25%;
+padding: 7px;
+border-radius: 3px;
+color: #999;
+svg{
+  margin-bottom: 5px;
+}
+input{
+  background-color: transparent;
+  border: none;
+  font-weight: 200;
+}
+`;
+const RightCol = styled.div`
+width: 25%;
+dis[lay: flex;
+justify-content: space-around;
+`;
 
 class Navbar extends Component{
   render(){
     return (
-      <div>
-        <h3><FaInstagram /> | Instaclone</h3>
+      <Wrapper>
+        <LeftCol><FaInstagram /> | Instaclone</LeftCol>
+       <MiddleCol>
         <FaSearch/><input type="text"/>
-        <div>
-          <FaCompass />
-          <FaHeartO />
-          <FaUser />
-        </div>
-      </div>
+        </MiddleCol>
+        <RightCol>
+          <FaCompass size={25}/>
+          <FaHeartO size={25}/>
+          <FaUser size={25}/>
+        </RightCol>
+      </Wrapper>
     );
   }
 }
